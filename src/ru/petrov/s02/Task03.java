76365a87cdab34c85b59fd46f8dc3778f8ce842b
Andrey Petrov <3314099@mail.ru>
2018-02-27 23:05:30 +0300

    S02-T03

---
+++ src/ru/petrov/s02/Task03.java
@@ -0,0 +1,21 @@
+package ru.petrov.s02;
+
+public class Task03 {
+
+    public static void main(String[] args) {
+        new Task03();
+    }
+
+    public Task03(){
+
+        int x ,y ;
+
+        x = 8;
+        y = 6;
+
+        System.out.println(Math.max(x, y));
+        System.out.println(Math.min(x, y));
+
+    }
+
+}
