fc966af5961e4fa09560360571977327198057bc
Andrey Petrov <3314099@mail.ru>
2018-02-28 12:20:14 +0300

    S02-T01/2

--- src/ru/petrov/s02/Task01.java
+++ src/ru/petrov/s02/Task01.java
@@ -1,5 +1,4 @@
 package ru.petrov.s02;
-import java.util.Scanner;
 
     public class Task01 {
 
@@ -10,37 +9,9 @@ import java.util.Scanner;
 
         public static int sum(int n) {
 
-
             int a = n % 10;
             int b = n / 10;
-            int sum = a + b;
-            return sum;
+            return a + b;
         }
 
     }
-
-/*
-
-//Нашел еще крутой вариант(очень мне понравился) - помог понять!!
-
-    public static void main(String[] args) {
-
-
-        int n = 5661;
-        int sum = 0;
-        sum(n, sum);
-    }
-
-        public static void sum(int n, int sum){
-
-        if(n!=0){
-            sum+=n%10;
-            sum(n/10, sum);
-
-        }
-        else System.out.println(sum);
-        }
-}
-*/
-
-
