50c1dde214f7b06ee54d25cc501a7d7e67ba4703
Andrey Petrov <3314099@mail.ru>
2018-02-28 18:35:07 +0300

    S02-T05

--- src/ru/petrov/s02/Task05.java
+++ src/ru/petrov/s02/Task05.java
@@ -3,19 +3,19 @@ package ru.petrov.s02;
 public class Task05 {
 
     public static void main(String[] args) {
-        val(3,7);
+        val(3, 7);
     }
 
     public static double val(double x, double y) {
 
-        x = Math.PI * Math.pow(x,2);
-        y = Math.pow(y,2);
+        x = Math.PI * Math.pow(x, 2);
+        y = Math.pow(y, 2);
 
-        if (x > y){
-            System.out.println( "Площадь круга " + x + " больше площади квадрата " + y);
+        if (x > y) {
+            System.out.println("Площадь круга " + x + " больше площади квадрата " + y);
             return y;
-        } else{
-            System.out.println( "Площадь квадрата " + x + " больше площади круга " + y);
+        } else {
+            System.out.println("Площадь квадрата " + x + " больше площади круга " + y);
             return x;
         }
     }
