70efa35069ea564b7cedc12730b449f0ab20ed8c
Andrey Petrov <3314099@mail.ru>
2018-02-28 20:29:29 +0300

    S02-T04/3

--- src/ru/petrov/s02/Task04.java
+++ src/ru/petrov/s02/Task04.java
@@ -3,18 +3,27 @@ package ru.petrov.s02;
 public class Task04 {
 
     public static void main(String[] args) {
-       val(1,30000);
+
+        int a = val(1, 30000);
+
+        if (a == 1) {
+            System.out.println("Значение в километрах больше значения в футах");
+        } else {
+            System.out.println("Значение в километрах меньше значения в футах");
+        }
     }
 
-    public static double val(double x, double y) {
+    public static int val(int x, int y) {
 
-        if (x > (y * 0.000305)){
-            System.out.println( y + " футов меньше " + x + " километров");
-            return y;
-        } else{
-            System.out.println( x +" колометров меньше " + y + " футов");
-            return x;
+        int a;
+
+        if (x > (y * 0.000305)) {
+            a = 1;
+        } else {
+            a = 0;
         }
+
+        return a;
     }
 
 }
