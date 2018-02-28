674e998bbfb5633e62d372a9b25ac88cbcc0529c
Andrey Petrov <3314099@mail.ru>
2018-02-28 19:46:22 +0300

    S02-T03

--- src/ru/petrov/s02/Task03.java
+++ src/ru/petrov/s02/Task03.java
@@ -1,33 +1,29 @@
 package ru.petrov.s02;
 
-
 public class Task03 {
 
     public static void main(String[] args) {
-    int x,y;
-    x = 5;
-    y = 6;
 
-        int max = max(x,y);
-        System.out.println(max);
-        int min = min(x,y);
-        System.out.println(min);
+        int a = compare(7, 5);
+
+        if (a == 1) {
+            System.out.println("Первое число больше второго");
+        } else if (a == 2) {
+            System.out.println("Числа равны");
+        } else {
+            System.out.println("Второе число больше первого");
+        }
+
     }
 
-    public static int max (int x, int y) {
-
-
-        int max = Math.max(x, y);
-
-        return max;
-    }
-
-    public static int min (int x, int y) {
-
-
-        int min = Math.min(x, y);
-
-        return min;
+    public static int compare(int x, int y) {
+        if (x > y) {
+            return 1;
+        } else if (x == y) {
+            return 2;
+        } else {
+            return 0;
+        }
     }
 
 }
