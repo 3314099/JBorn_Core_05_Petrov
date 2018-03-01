83081425f1c7b5f8c765caf0d95368f8e3e57b07
Andrey Petrov <3314099@mail.ru>
2018-03-01 14:19:57 +0300

    S02-T14/1

--- src/ru/petrov/s02/Task14.java
+++ src/ru/petrov/s02/Task14.java
@@ -24,31 +24,9 @@ public class Task14 {
         for (int k = i; k > 0; k--) {
             str = str + "*";
         }
+
         return str;
 
     }
 
 }
-
-
-/*
-    //S02-T14.    14.    Вывести треугольник в консоль. Где кол-во звездочек на последнем уровне передается параметром
-    // *
-    // **
-    // …….
-    // *****************
-
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введите число:");
-        int a = sc.nextInt();
-        sc.close();
-
-        for (int i = 0; i <= a; i++) {
-            for (int k = i; k > 0; k--) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-*/
-
