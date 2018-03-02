b0bca6b7cccdc6767163e41255f8a5f724d206e0
Andrey Petrov <3314099@mail.ru>
2018-03-02 17:10:08 +0300

    S02-T04/5

--- src/ru/petrov/s02/Task04.java
+++ src/ru/petrov/s02/Task04.java
@@ -4,24 +4,18 @@ public class Task04 {
 
     public static void main(String[] args) {
 
-        int com = comparison(1, 3000);
+        boolean com = comparison(1, 3000);
 
-        if (com == 1) {
+        if (com) {
             System.out.println("Значение в километрах больше значения в футах");
         } else {
             System.out.println("Значение в километрах меньше значения в футах");
         }
     }
 
-    public static int comparison(int km, int foot) {
+    public static boolean comparison(int km, int foot) {
 
-        int com;
-
-        if (km > (foot * 0.000305)) {
-            com = 1;
-        } else {
-            com = 0;
-        }
+        boolean com = km > (foot * 0.000305);
 
         return com;
     }
