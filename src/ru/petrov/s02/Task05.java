d2db0c5f4da9d4d3a0b42c586a013720630c182a
Andrey Petrov <3314099@mail.ru>
2018-03-02 22:23:57 +0300

    S02-T05

--- src/ru/petrov/s02/Task05.java
+++ src/ru/petrov/s02/Task05.java
@@ -3,10 +3,12 @@ package ru.petrov.s02;
 public class Task05 {
 
     public static void main(String[] args) {
-        int com = comparison(3, 7);
+        int com = comparison(3.94, 7);
 
         if (com == 1) {
             System.out.println("Площадь круга больше площади квадрата");
+        } else if (com == 2) {
+            System.out.println("Площадь квадрата и площадь круга равны");
         } else {
             System.out.println("Площадь квадрата больше площади круга");
         }
@@ -21,12 +23,12 @@ public class Task05 {
 
         if (radius > side) {
             com = 1;
+        } else if (radius == side) {
+            com = 2;
         } else {
             com = 0;
         }
 
         return com;
-
     }
-
 }
