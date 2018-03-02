95170e6b62106abfd8a56450f95c0645d0357d94
Andrey Petrov <3314099@mail.ru>
2018-03-02 22:40:35 +0300

    S02-T07/1

--- src/ru/petrov/s02/Task07.java
+++ src/ru/petrov/s02/Task07.java
@@ -4,9 +4,9 @@ public class Task07 {
 
     public static void main(String[] args) {
 
-        int pal = palindrome(232);
+        boolean pal = palindrome(435);
 
-        if (pal == 1) {
+        if (pal == true) {
             System.out.println("Число является палиндромом");
         } else {
             System.out.println("Число не является палиндромом");
@@ -14,26 +14,9 @@ public class Task07 {
 
     }
 
-    public static int palindrome(int x) {
+    public static boolean palindrome(int x) {
 
-        int array[] = new int[3];
-
-        for (int i = 0; i <= 2; i++) {
-            int div = x % 10;
-            array[i] = div;
-            x = x / 10;
-        }
-
-        int pal = 1;
-        int k = 2;
-
-        for (int i = 0; i <= 2; i++) {
-            if (array[i] != array[k]) {
-                pal = 0;
-            }
-
-            k--;
-        }
+        boolean pal = (x / 100 % 10 == x % 10);
 
         return pal;
 
