654117dca470a090b9261ee747a793ea02e5cc75
Andrey Petrov <3314099@mail.ru>
2018-03-04 00:33:54 +0300

    S02-T05

--- src/ru/petrov/s02/Task05.java
+++ src/ru/petrov/s02/Task05.java
@@ -3,6 +3,7 @@ package ru.petrov.s02;
 public class Task05 {
 
     public static void main(String[] args) {
+
         int com = comparison(3.94, 7);
 
         if (com == 2) {
@@ -12,12 +13,13 @@ public class Task05 {
         } else {
             System.out.println("Площадь квадрата больше площади круга");
         }
+
     }
 
     public static int comparison(double radius, double side) {
 
         double circleArea = Math.PI * Math.pow(radius, 2);
-        double squareArea= Math.pow(side, 2);
+        double squareArea = Math.pow(side, 2);
 
         int com;
 
