1d2a21988129b3fb306a642b66222e280d7e3afc
Andrey Petrov <3314099@mail.ru>
2018-03-04 00:32:50 +0300

    S02-T05

--- src/ru/petrov/s02/Task05.java
+++ src/ru/petrov/s02/Task05.java
@@ -5,9 +5,9 @@ public class Task05 {
     public static void main(String[] args) {
         int com = comparison(3.94, 7);
 
-        if (com == 1) {
+        if (com == 2) {
             System.out.println("Площадь круга больше площади квадрата");
-        } else if (com == 2) {
+        } else if (com == 1) {
             System.out.println("Площадь квадрата и площадь круга равны");
         } else {
             System.out.println("Площадь квадрата больше площади круга");
@@ -16,15 +16,15 @@ public class Task05 {
 
     public static int comparison(double radius, double side) {
 
-        radius = Math.PI * Math.pow(radius, 2);
-        side = Math.pow(side, 2);
+        double circleArea = Math.PI * Math.pow(radius, 2);
+        double squareArea= Math.pow(side, 2);
 
         int com;
 
-        if (radius > side) {
-            com = 1;
-        } else if (radius == side) {
+        if (circleArea > squareArea) {
             com = 2;
+        } else if (circleArea == squareArea) {
+            com = 1;
         } else {
             com = 0;
         }
