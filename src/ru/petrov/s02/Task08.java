d7405cde0522fd5dc33b6bdcdf4adcf03ca82f07
Andrey Petrov <3314099@mail.ru>
2018-03-04 01:11:00 +0300

    S02-T08

--- src/ru/petrov/s02/Task08.java
+++ src/ru/petrov/s02/Task08.java
@@ -4,7 +4,7 @@ public class Task08 {
 
     public static void main(String[] args) {
 
-        boolean check = recurringNumberCheck(243);
+        boolean check = recurringNumberCheck(344);
 
         if (check == true) {
             System.out.println("Есть повторяющееся число");
@@ -16,10 +16,7 @@ public class Task08 {
 
     public static boolean recurringNumberCheck(int x) {
 
-        boolean check;
-        check = ((x / 100 % 10 == x % 10) || (x / 10 % 10 == x % 10) || (x / 100 % 10 == x / 10 % 10));
-
-        return check;
+        return ((x / 100 == x / 10 % 10) || (x / 100 == x % 10) || (x / 10 % 10 == x % 10));
 
     }
 
