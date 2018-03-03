a41697f6fd3676a8038753a145a13e44a9e02726
Andrey Petrov <3314099@mail.ru>
2018-03-03 03:05:37 +0300

    S03-T02

---
+++ src/ru/petrov/s03/Task02.java
@@ -0,0 +1,33 @@
+package ru.petrov.s03;
+
+import java.util.Scanner;
+
+public class Task02 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите слово:");
+        String word = sc.nextLine();
+        sc.close();
+
+        String str = printStars(word);
+        System.out.println(str);
+
+    }
+
+    public static String printStars(String word) {
+
+        int len = word.length();
+        String str;
+        String stars = "";
+
+        for (int i = 1; i <= len; i++) {
+            stars = stars + "*";
+        }
+        str = stars + word + stars;
+        return str;
+
+    }
+
+}
